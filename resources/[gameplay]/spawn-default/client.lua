d6a0731045b41434aa55981d0199edc7433aaf8b
puarudz <puarudz@users.noreply.github.com>
2026-07-13 06:35:41 +0700

    fix spawn: freeze ped, ground Z, re-get ped sau SetPlayerModel

--- resources/[gameplay]/spawn-default/client.lua
+++ resources/[gameplay]/spawn-default/client.lua
@@ -2,25 +2,35 @@ local defaultModel = 'mp_m_freemode_01'
 local spawnPos = vector4(456.09, 6565.5, 27.0, 257.35)
 local firstSpawn = true
 
-AddEventHandler('playerSpawned', function()
-    Wait(100)
-    local ped = PlayerPedId()
+-- Pre-load model
+local modelHash = GetHashKey(defaultModel)
+RequestModel(modelHash)
+while not HasModelLoaded(modelHash) do Wait(10) end
 
-    -- Set model on first spawn only
+AddEventHandler('playerSpawned', function()
+    -- Freeze to prevent running in place
+    local ped = PlayerPedId()
+    FreezeEntityPosition(ped, true)
+
     if firstSpawn then
         firstSpawn = false
-        local model = GetHashKey(defaultModel)
-        if GetEntityModel(ped) ~= model then
-            RequestModel(model)
-            while not HasModelLoaded(model) do Wait(10) end
-            SetPlayerModel(PlayerId(), model)
+        if GetEntityModel(ped) ~= modelHash then
+            SetPlayerModel(PlayerId(), modelHash)
+            -- re-get ped after model change
+            ped = PlayerPedId()
             SetPedDefaultComponentVariation(ped)
-            SetModelAsNoLongerNeeded(model)
         end
     end
 
-    Wait(500)
-    SetEntityCoords(ped, spawnPos.x, spawnPos.y, spawnPos.z, false, false, false, false)
+    -- Get ground Z and teleport
+    local found, groundZ = GetGroundZFor_3dCoord(spawnPos.x, spawnPos.y, 1000.0, false)
+    if not found then groundZ = spawnPos.z end
+    local finalZ = math.max(groundZ, spawnPos.z) + 0.5
+
+    SetEntityCoords(ped, spawnPos.x, spawnPos.y, finalZ, false, false, false, false)
     SetEntityHeading(ped, spawnPos.w)
-    TriggerEvent('chat:addMessage', { args = { '^2[Spawn] ^7Chào mừng đến Samael City!' } })
+
+    Wait(200)
+    FreezeEntityPosition(ped, false)
+    TriggerEvent('chat:addMessage', { args = { '^2[Spawn] ^7Chao mung den Samael City!' } })
 end)
